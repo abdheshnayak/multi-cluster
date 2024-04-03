9b9b767de32cd37d9501987dc62cef70060816cb
Abdhesh Nayak <abdheshnayak@gmail.com>
2024-04-03 15:42:03 +0530

    :tada: Init

---
+++ constants/main.go
@@ -0,0 +1,7 @@
+package constants
+
+const (
+	IP_RANGE      = "10.13.0.0"
+	ReconDuration = 5
+	ExpiresIn     = 8
+)
---
+++ mpkg/wg/gen-utils.go
@@ -0,0 +1,39 @@
+package wg
+
+import (
+	"github.com/seancfoley/ipaddress-go/ipaddr"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+)
+
+func Ptr[T any](t T) *T {
+	return &t
+}
+
+func GenerateWgKeys() ([]byte, []byte, error) {
+	key, err := wgtypes.GenerateKey()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return []byte(key.PublicKey().String()), []byte(key.String()), nil
+}
+
+func GeneratePublicKey(privateKey string) ([]byte, error) {
+	key, err := wgtypes.ParseKey(privateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return []byte(key.PublicKey().String()), nil
+}
+
+func GetRemoteDeviceIp(deviceOffcet int64) ([]byte, error) {
+	deviceRange := ipaddr.NewIPAddressString("10.13.0.0/16")
+
+	if address, addressError := deviceRange.ToAddress(); addressError == nil {
+		increment := address.Increment(deviceOffcet)
+		return []byte(ipaddr.NewIPAddressString(increment.GetNetIP().String()).String()), nil
+	} else {
+		return nil, addressError
+	}
+}
